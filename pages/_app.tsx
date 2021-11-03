9f76ea00d2488d7d400d62afa238e1245763d6c5
m2vi <iuvo@gmx.at>
2021-11-03 19:27:59 +0100

    👽v

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import '../styles/globals.css';
 import 'react-toastify/dist/ReactToastify.css';
-import '../styles/react-toggle.css';
 import type { AppProps } from 'next/app';
 import { ToastContainer } from 'react-toastify';
 
